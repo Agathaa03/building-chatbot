1202c20640041d1a27be0eb100d29a78d7afd95d
Agathaa03 <167002593+Agathaa03@users.noreply.github.com>
2025-09-25 01:03:26 +0800

    Update script.js
    Signed-off-by: Agathaa03 <167002593+Agathaa03@users.noreply.github.com>

---
+++ script.js
@@ -0,0 +1,24 @@
+{
+  "name": "summer-chatbot-project",
+  "version": "1.0.0",
+  "description": "A simple chatbot for summer practice project",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js",
+    "dev": "nodemon index.js",
+    "test": "jest"
+  },
+  "keywords": ["chatbot", "ai", "summer-project"],
+  "author": "Your Name",
+  "license": "MIT",
+  "dependencies": {
+    "express": "^4.18.2",
+    "socket.io": "^4.7.2",
+    "axios": "^1.5.0",
+    "dotenv": "^16.3.1"
+  },
+  "devDependencies": {
+    "nodemon": "^3.0.1",
+    "jest": "^29.6.2"
+  }
+}
